765f0ebf29786df99dfe4f112c6fcd4afdd972e7
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-10-27 11:34:40 +0000

    Deploy raegar/react-app-with-tests to github.com/raegar/react-app-with-tests.git:gh-pages

---
+++ precache-manifest.1a3d975cb27c42e7e81e4874a86b41d5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "977fd323cf6dc8adfe33",
+    "url": "/react-app-with-tests/static/css/main.4567239b.chunk.css"
+  },
+  {
+    "revision": "977fd323cf6dc8adfe33",
+    "url": "/react-app-with-tests/static/js/main.9383a39b.chunk.js"
+  },
+  {
+    "revision": "7683c0cd3131d886786b",
+    "url": "/react-app-with-tests/static/js/runtime~main.c271cb49.js"
+  },
+  {
+    "revision": "4f2cec41e716444d6eca",
+    "url": "/react-app-with-tests/static/js/2.2a0f4af3.chunk.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-app-with-tests/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "4a08fb8d37e6add241c0380d2a2e023a",
+    "url": "/react-app-with-tests/index.html"
+  }
+];
